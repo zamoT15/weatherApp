01d749fc6f5b97d582ee5a95065a53d6fd644b42
Tomasz <tomasz.sitko15@gmail.com>
2020-10-16 18:58:50 +0200

    navigation arrow
    added appbar button to back

--- app/src/main/java/com/android/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/android/example/weatherapp/MainActivity.kt
@@ -3,23 +3,21 @@ package com.android.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.android.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        drawerLayout = binding.drawerLayout
-
         val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
 
-       NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
-
-       NavigationUI.setupWithNavController(binding.navView, navController)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
